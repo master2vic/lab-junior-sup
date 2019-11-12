f2c2b2ae1efd4e8bb7368fc99f9edc2e4f7553fd
shiYy <1604989334@qq.com>
2019-11-12 17:42:42 +0800

    almost finish chapter 18

---
+++ os/homework/chapter16/op.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# created by hansz
+import os
+
+ASIZE = ["16","64k","32m","1g"]
+
+PSIZE = ["16","64k","32m","1g"]
+
+for asize in ASIZE:
+    for psize in PSIZE:
+        if ASIZE.__le__:
+            pass
+os.system("python2 ./")
